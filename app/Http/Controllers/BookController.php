b0e60a701225b6dc31e05bcd0eebea77f8b9a2d7
Mihir <patelm48@student.wpunj.edu>
2016-04-12 20:03:17 -0400

    adding seeds/migrations

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -10,21 +10,21 @@ class BookController extends Controller {
     * Responds to requests to GET /books
     */
     public function getIndex() {
-        return 'List all the books';
+        return view('books.index');
     }
 
     /**
      * Responds to requests to GET /books/show/{id}
      */
-    public function getShow($id) {
-        return 'Show book: '.$id;
+    public function getShow($title = null) {
+        return view('books.show')->with('title',$title);
     }
 
     /**
      * Responds to requests to GET /books/create
      */
     public function getCreate() {
-        return 'Form to create a new book';
+        return view('books.create');
     }
 
     /**
---
+++ resources/views/books/create.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.master')
+
+
+@section('title')
+    Book Index
+@stop
+
+
+{{--
+This `head` section will be yielded right before the closing </head> tag.
+Use it to add specific things that *this* View needs in the head,
+such as a page specific stylesheets.
+--}}
+@section('head')
+    <link href="/css/books/show.css" type='text/css' rel='stylesheet'>
+@stop
+
+
+@section('content')
+    <h1>Welcome, this is the book create page!</h1>
+    <p>Input form will be displayed soon!</p>
+@stop
+
+
+{{--
+This `body` section will be yielded right before the closing </body> tag.
+Use it to add specific things that *this* View needs at the end of the body,
+such as a page specific JavaScript files.
+--}}
+@section('body')
+    <script src="/js/books/show.js"></script>
+@stop
